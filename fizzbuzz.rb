7bdf1391ae4d9136e8e399bddaf4533431e68e4e
Sawandi Wilson <sawandiwilson@gmail.com>
2020-06-10 17:36:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(int)
-if int % 3 == 0
+if int % 5 and % 3 == 0
   "Fizzbuzz"
-  elsif int % 5 == 0
+  elsif int % 3 == 0
+  "Fizz"
+  elsif int % 5 == 
   "Buzz"
-  elsif int % 4 == 2
-  "nil"
-else int % 5 and % 3 == 0
-"Fizzbuzz"
+
 end
 end
